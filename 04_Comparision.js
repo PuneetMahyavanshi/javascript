18e94425303c1d14215f7ce664e27c635c662dff
PuneetMahyavanshi <82257931+PuneetMahyavanshi@users.noreply.github.com>
2024-08-18 11:13:17 +0000

    note in comparision file

--- 04_Comparision.js
+++ 04_Comparision.js
@@ -2,8 +2,9 @@
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
-
+console.log(null >= 0);
 // The reason is that an equality check == and comparision > <>= <= works differently
 // Comparision convert the null to 0 and treating it as 0 so that is why the 3 one is true 
 
-
+//mostly we have to stay away from such kind of comparision, it is ok if we are comparing 
+//with same datatype but not ok of these kind of comparision null or string to number comparision
