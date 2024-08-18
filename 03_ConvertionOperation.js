686c66f10a1f5f48f5d26084c5d6678c6fb9495c
PuneetMahyavanshi <82257931+PuneetMahyavanshi@users.noreply.github.com>
2024-08-18 05:43:05 +0000

    convert other types to number

---
+++ 03_ConvertionOperation.js
@@ -0,0 +1,31 @@
+let score = "55"
+
+console.log(score);
+console.log(typeof score);
+
+let numberScore = Number(score);
+console.log(numberScore);
+console.log(typeof numberScore);
+
+// 55 => number 55
+
+let highScore = "70highScore"
+console.log(typeof highScore);
+let nHighScore = Number(highScore);
+console.log(typeof nHighScore);
+console.log(nHighScore);
+
+//"70highScore" => will get converted to number but as NaN
+
+let isHighScore = true 
+console.log(typeof isHighScore);
+let bHighScore = Number(isHighScore)
+console.log(typeof bHighScore);
+console.log(bHighScore);
+
+//true => 1; false => 0
+
+
+
+
+
